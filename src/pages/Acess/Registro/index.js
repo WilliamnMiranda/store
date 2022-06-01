e6acacb6fc5027539e2b17d0aa6287d667860761
william <williamnnmiranda@gmail.com>
2022-06-01 20:45:57 -0300

    adicionado register

--- src/pages/Acess/Registro/index.js
+++ src/pages/Acess/Registro/index.js
@@ -1,9 +1,29 @@
 import React from 'react'
-import { Container,Tittle } from './style'
+import { Container, Tittle, ContainerFormulario, ContainerInput,ButtonSubmit, Recuperation, RecuperationText,
+  SocialsLogins,SubTittle,ButtonFacebook,ButtonGoogle,ContainerButtons } from './style'
+  import InputValue from '../../../components/Form/input'
 const Register = () => {
   return (
     <Container>
       <Tittle>QUERO ME CADASTRAR</Tittle>
+      <ContainerFormulario>
+        <ContainerInput>
+          <InputValue placeholder={'E-mail *'}/>
+        </ContainerInput>
+        <ContainerInput>
+          <InputValue placeholder={'Crie sua senha *'} />
+        </ContainerInput>
+        <ContainerInput>
+          <InputValue placeholder={'Confirme a senha *'}/>
+        </ContainerInput>
+        <ContainerInput>
+          <InputValue placeholder={'CPF ou CNPJ *'}/>
+        </ContainerInput>
+        <ContainerInput>
+          <InputValue placeholder={'CEP *'}/>
+        </ContainerInput>
+        <ButtonSubmit> ENTRAR </ButtonSubmit>
+      </ContainerFormulario>
     </Container>
   )
 }
