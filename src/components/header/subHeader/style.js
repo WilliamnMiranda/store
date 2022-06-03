bc4853dee3ee3511e857efe0bdd13e7058334886
william <williamnnmiranda@gmail.com>
2022-06-03 02:11:40 -0300

    ajuste responsivo no subheader

--- src/components/header/subHeader/style.js
+++ src/components/header/subHeader/style.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
   export const ContainerSubMenu = styled.aside`
     width: 100%;
     background-color: rgb(255, 101, 0);
-    @media(max-width: 1250px) {
+    @media(max-width: 800px) {
       display: none;
   }
 `
@@ -11,6 +11,9 @@ import styled from 'styled-components'
   export const ContainerMenu = styled.nav`
     width: 80%;
     margin: 0 auto;
+    @media(max-width: 1500px) {
+      width: 100%;
+  }
 `
 
   export const ContainerLista = styled.ul`
