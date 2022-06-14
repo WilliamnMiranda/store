1fc0d142542177f6a0de232e10fdd0f71d724161
william <williamnnmiranda@gmail.com>
2022-06-14 18:50:07 -0300

    add menu list

--- src/components/Drawer/index.js
+++ src/components/Drawer/index.js
@@ -1,21 +1,42 @@
-import React from 'react'
-import {ContainerDrawer,DrawerComponent,DrawerNav,DrawerList,TituloDrawer,CloseDrawer} from './style'
-import { DrawerContext } from '../../contexts/drawer'
+import React from "react";
+import { FaUserAlt,FaShoppingCart,FaHeart,FaHeadset, FaHotjar,FaBolt,FaGem } from "react-icons/fa";
+import {
+  ContainerDrawer,
+  DrawerComponent,
+  DrawerNav,
+  DrawerList,
+  TituloDrawer,
+  CloseDrawer,
+  DrawerItem,
+  ButtonLogin,
+  ContainerLogin,
+} from "./style";
+import { DrawerContext } from "../../contexts/drawer";
 const Drawer = () => {
   const { setStatusMenu } = React.useContext(DrawerContext);
   return (
     <ContainerDrawer>
-        <DrawerComponent>
-            <CloseDrawer onClick={()=> setStatusMenu(false)}> X </CloseDrawer>
-            <TituloDrawer>Ola.Faca seu login</TituloDrawer>
-            <DrawerNav>
-                <DrawerList>
-
-                </DrawerList>
-            </DrawerNav>
-        </DrawerComponent>
+      <DrawerComponent>
+        <CloseDrawer onClick={() => setStatusMenu(false)}> X </CloseDrawer>
+        <TituloDrawer>Ola.Faca seu login</TituloDrawer>
+        <DrawerNav>
+          <DrawerList>
+            <DrawerItem> <FaUserAlt /> Minha conta </DrawerItem>
+            <DrawerItem> <FaShoppingCart /> Meus Pedidos </DrawerItem>
+            <DrawerItem> <FaHeart /> Favoritos </DrawerItem>
+            <DrawerItem> <FaHeadset /> Atendimento </DrawerItem>
+            <DrawerItem> <FaHotjar/> Mais Procurados </DrawerItem>
+            <DrawerItem> <FaBolt /> Oferta do dia </DrawerItem>
+            <DrawerItem> <FaGem /> Seja Premium </DrawerItem>
+            <ContainerLogin>
+              <ButtonLogin> Login </ButtonLogin>
+              <div> Cadastro </div>
+            </ContainerLogin>
+          </DrawerList>
+        </DrawerNav>
+      </DrawerComponent>
     </ContainerDrawer>
-  )
-}
+  );
+};
 
-export default Drawer
+export default Drawer;
--- src/components/Drawer/style.js
+++ src/components/Drawer/style.js
@@ -1,5 +1,5 @@
-import styled from 'styled-components'
-import { keyframes } from 'styled-components';
+import styled from "styled-components";
+import { keyframes } from "styled-components";
 const animMenu = keyframes`
     0% {
      transform: translateX(-360px);
@@ -9,30 +9,67 @@ const animMenu = keyframes`
         transform: translateY(0px);
     }
 `;
-export const ContainerDrawer= styled.div`
-    width: 100%;
-    background-color: rgba(0,0,0,0.3);
-    height: 100%;
-    position: absolute;
-    z-index: 10;
-`
+export const ContainerDrawer = styled.div`
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.3);
+  height: 100%;
+  position: absolute;
+  z-index: 10;
+`;
 
-export const TituloDrawer = styled.h1``
+export const TituloDrawer = styled.h1`
+    margin-bottom: 20px;
+`;
 export const DrawerComponent = styled.aside`
-    position: fixed;
-    padding: 30px 20px;
-    animation: ${animMenu} .5s;
-    width: 350px;
-    height: 100%;
-    background-color: rgb(0, 96, 177);
-`
-export const DrawerNav = styled.nav``
-export const DrawerList = styled.ul``
+  position: fixed;
+  padding: 30px;
+  animation: ${animMenu} 0.5s;
+  width: 350px;
+  height: 100%;
+  background-color: rgb(0, 96, 177);
+`;
+export const DrawerNav = styled.nav``;
+export const DrawerList = styled.ul``;
+export const DrawerItem = styled.li`
+    color: white;
+    margin-bottom: 30px;
+`;
 export const CloseDrawer = styled.div`
-        position: absolute;
-        top: 4%;
-        left:110%;
+  position: absolute;
+  top: 4%;
+  left: 110%;
+  color: white;
+  font-size: 1.5em;
+  cursor: pointer;
+`;
+export const ButtonLogin = styled.button`
+  width: 100%;
+  height: 48px;
+  display: flex;
+  -webkit-box-pack: center;
+  justify-content: center;
+  -webkit-box-align: center;
+  align-items: center;
+  border: 1px solid rgb(255, 101, 0);
+  background-color: rgb(255, 101, 0);
+  user-select: none;
+  color: rgb(255, 255, 255);
+  font-size: 18px;
+  font-weight: 700;
+  text-transform: uppercase;
+  transition: background-color 0.3s ease 0s;
+  border-radius: 0.25rem;
+`;
+
+export const ContainerLogin = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    div{
+        text-transform: uppercase;
         color:white;
-        font-size:1.5em;
-        cursor: pointer;
+        margin-top: 20px;
+        font-size: 1.4em;
+    }
 `
