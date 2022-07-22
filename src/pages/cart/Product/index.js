38cb9afb6ea9adb4348525f9585bdc113ccce9ba
william nascimento <williamnnmiranda@gmail.com>
2022-07-22 14:27:15 -0300

    finalizado perfil de items no carrinho

--- src/pages/cart/Product/index.js
+++ src/pages/cart/Product/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import { FaChevronLeft,FaChevronRight } from "react-icons/fa";
-import { ContainerProduto,ContainerInfosProdutos,FotoProduto,InfoProduto,Nome,QuantidadeProduto,PrecoAvista,BotaodiminuirQuantidade,BotaoAumentarQuantidade,QuantidadeDoProduto,Remove } from './style'
+import { FaChevronLeft,FaChevronRight,FaTrash } from "react-icons/fa";
+import { ContainerProduto,ContainerInfosProdutos,FotoProduto,InfoProduto,Nome,QuantidadeProduto,PrecoAvista,BotaodiminuirQuantidade,BotaoAumentarQuantidade,
+   QuantidadeDoProduto,Remove,Marca,Valor } from './style'
 const Produto = ({product}) => {
   return (
     <ContainerProduto>
        <ContainerInfosProdutos>
             <FotoProduto src={product.photo}/>
             <InfoProduto> 
+                <Marca>Hyperx</Marca>
                 <Nome> {product.name} </Nome>
+                <Valor>Parcelado no cartao em ate 10x sem juros : 1.434,21</Valor>
             </InfoProduto>
             <QuantidadeProduto>
                <p>quant</p>
@@ -16,9 +19,12 @@ const Produto = ({product}) => {
                   <QuantidadeDoProduto> 5 </QuantidadeDoProduto>
                   <BotaoAumentarQuantidade> <FaChevronRight/> </BotaoAumentarQuantidade>
                </div>
-               <Remove>REMOVER</Remove>
+               <Remove> <FaTrash style={{marginRight:'5px'}} /> REMOVER</Remove>
             </QuantidadeProduto>
-            <PrecoAvista></PrecoAvista>
+            <PrecoAvista>
+               <p>Preco a vistano pix</p>
+               <div> {product.price.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})} </div>
+            </PrecoAvista>
         </ContainerInfosProdutos> 
     </ContainerProduto>
   )
--- src/pages/cart/Product/style.js
+++ src/pages/cart/Product/style.js
@@ -4,7 +4,8 @@ export const ContainerProduto = styled.div`
   margin: 0 auto;
   margin-bottom: 15px;
   border-top: 1px solid rgb(222, 224, 228);
-  height: 100px;
+  height: 80px;
+  padding: 5px;
   width: 100%;
 `
 export const ContainerInfosProdutos = styled.div`
@@ -12,13 +13,16 @@ export const ContainerInfosProdutos = styled.div`
   height: 100%;
 `
 export const FotoProduto = styled.img`
-  width: 15%;
+  width: 10%;
   height: 100%;
   background-color: grey;
 `
 export const InfoProduto = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
   height: 100%;
-  width: 50%;
+  width: 55%;
   padding:10px;
 `
 export const Nome = styled.div`
@@ -31,12 +35,13 @@ export const Nome = styled.div`
     }
 `
 export const QuantidadeProduto = styled.div`
+  padding:10px;
   height: 100%;
   width: 15%;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
   p{
     font-size: 0.8em;
     margin-bottom: 5px;
@@ -48,24 +53,55 @@ export const QuantidadeProduto = styled.div`
     background-color: transparent;
     border: none;
     color: rgb(255, 101, 0);
+    display: flex;
+    align-items: center;
     cursor: pointer;
+    span{
+      margin-right: 0px;
+    }
   }
 `
 export const BotaodiminuirQuantidade = styled.button``
 export const BotaoAumentarQuantidade = styled.button``
 export const QuantidadeDoProduto = styled.div`
-font-size: 0.9em;
-position: relative;
-top:-10%;
-padding: 5px;
+  font-size: 0.9em;
+  position: relative;
+  top:-10%;
+  padding: 5px;
 `
 export const PrecoAvista = styled.div`
+  padding:10px;
   height: 100%;
   width: 20%;
-  background-color: blue;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-end;
+  p{
+    font-size: 0.8em;
+    color: rgb(127, 133, 141);
+    font-family: Poppins, sans-serif !important;
+  }
+  div{
+    font-size: 1.2em;
+    font-weight: bold;
+    color: rgb(255, 101, 0);
+    margin-top: 10px;
+  }
 `
 export const Remove = styled.button`
   font-size: 0.6em!important;
   color: rgb(231, 38, 38)!important;
   font-weight: bold;
 `
+export const Marca = styled.p`
+  font-size: 0.8em;
+  color: rgb(127, 133, 141);
+  font-family: Poppins, sans-serif !important;
+`
+export const Valor = styled.div`
+  font-size: 0.8em;
+  color: rgb(127, 133, 141);
+  font-family: Poppins, sans-serif !important;
+`
+
