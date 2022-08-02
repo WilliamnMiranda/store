19667ea275bd987925b9b36100e24f755c3170eb
william nascimento <williamnnmiranda@gmail.com>
2022-08-02 20:22:37 -0300

    adicionado callback para resolver renderizacoes a mais

--- src/contexts/cart.js
+++ src/contexts/cart.js
@@ -4,37 +4,36 @@ export const CartContext = React.createContext();
 
 export const CartStore = ({ children }) => {
   const [cart, setCart] = React.useState([]);
-  const att = () => {
-    getItemsFromCart()
-    getItemsFromCart()
-  }
-  const getItemsFromCart =async () => {
+
+  const getItemsFromCart = React.useCallback(async () => {
     const productsCart = await cartServices.get();
     setCart(productsCart);
-  }
+  },[])
 
   const deleteProduct = async(id) => {
    cartServices.delete(id)
-   att()
+   getItemsFromCart()
   }
   const addToCart = async (product) => {
     const checkItemCart = cart.some((item)=> item.product._id === product._id)
     if(checkItemCart) return 
     cartServices.post(product)
-    att()
+    getItemsFromCart()
   }
   const updateItem = async (items) => {
     const newProducts = await cartServices.patch(items)
-    att()
+    getItemsFromCart()
   }
   const deleteAll = async () =>{
       const product = await cartServices.deleteAll();
-      att()
+      getItemsFromCart()
   }
 
   React.useEffect(()=>{
     getItemsFromCart()
-  },[])
+  },[getItemsFromCart])
+
+  
  
   return <CartContext.Provider value={{ cart,addToCart,deleteAll,getItemsFromCart,deleteProduct,updateItem}}>{children}</CartContext.Provider>
 }
